3af7edc99af9f5a1adc3cb5137033be84d0167b7
prhineh1 <rhinehartphillip@gmail.com>
2023-04-28 09:26:24 -0400

    philosophers returns boolean

--- src/crawlers/philosophers.ts
+++ src/crawlers/philosophers.ts
@@ -11,17 +11,19 @@ import jsdom = require("jsdom");
 
 const { JSDOM } = jsdom;
 
-async function appendToCsv(data: string[]): Promise<string[]> {
-  const csv = await getFile(join(cwd(), "philosopher.csv"), "id,name");
-  const pairedData = data.map((name, idx) => `${idx + 1},${name}`);
-  const csvData = pairedData.join("\n");
-  await csv.appendFile(csvData);
-  await csv.close();
+async function appendToCsv(data: string): Promise<boolean> {
+  try {
+    const csv = await getFile(join(cwd(), "philosopher.csv"), "id,name");
+    await csv.appendFile(data);
+    await csv.close();
 
-  return pairedData;
+    return true;
+  } catch {
+    return false;
+  }
 }
 
-async function getNames() {
+export async function getNames(): Promise<boolean> {
   const links = await getLinks();
   const names: string[] = [];
 
@@ -45,11 +47,10 @@ async function getNames() {
     }
   }
 
-  const pairs = await appendToCsv(names);
+  const pairs = names.map((name, idx) => `${idx + 1},${name}`);
   createMapping(
     createEntityToIdData(pairs),
     join(cwd(), "/src/generated/philToId.ts")
   );
+  return await appendToCsv(pairs.join("\n"));
 }
-
-getNames();
