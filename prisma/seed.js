f6f2b19f01614e620fccdc92a2af0e68d0e89c68
prhineh1 <rhinehartphillip@gmail.com>
2023-05-05 16:21:33 -0400

    docker updates

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,8 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-const dockerSeedPath = "/media/seeds/"
-
 const { philosopher } = await import("./seeds/philosopher.js");
 await prisma.philosopher.createMany({
     data: philosopher 
