aee1003266ab6a2a6cdef4864eff2079cfc4a991
prhineh1 <rhinehartphillip@gmail.com>
2023-05-09 13:23:42 -0400

    use cockroachDB locally

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 import { Prisma, PrismaClient } from "@prisma/client";
-import { ServerResponse, IncomingMessage } from "http";
+import { ServerResponse, IncomingMessage, createServer } from "http";
 import { quotesAPI } from "./api/quote.js";
 
 export type PrismaType = PrismaClient<
@@ -13,43 +13,28 @@ export type HttpResponseType = ServerResponse<IncomingMessage> & {
 };
 const prisma = new PrismaClient();
 
-const options = process.env.ISPRODUCTION
-  ? {}
-  : {
-      key: readFileSync("key.pem"),
-      cert: readFileSync("cert.pem"),
-    };
+const server = createServer((req, res) => {
+  const url = new URL(req.url ?? "", `https://${req.headers.host}`);
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Cache-Control", "public, max-age=604800");
 
-const protocol = process.env.ISPRODUCTION
-  ? await import("http")
-  : await import("https");
-
-//@ts-ignore
-const server = protocol.createServer(
-  options,
-  (req: IncomingMessage, res: HttpResponseType) => {
-    const url = new URL(req.url ?? "", `https://${req.headers.host}`);
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Cache-Control", "public, max-age=604800");
-
-    // only service "GET" requests
-    if (req.method !== "GET") {
-      res.writeHead(405);
-      res.end();
-      return;
-    }
-
-    switch (url.pathname) {
-      case "/api/quotes":
-        quotesAPI(url, res, prisma);
-        break;
-      default:
-        res.writeHead(404);
-        res.end();
-        break;
-    }
+  // only service "GET" requests
+  if (req.method !== "GET") {
+    res.writeHead(405);
+    res.end();
+    return;
   }
-);
+
+  switch (url.pathname) {
+    case "/api/quotes":
+      quotesAPI(url, res, prisma);
+      break;
+    default:
+      res.writeHead(404);
+      res.end();
+      break;
+  }
+});
 
 server.listen(8080, () => {
   console.log("HTTPS server listening on port 8080");
